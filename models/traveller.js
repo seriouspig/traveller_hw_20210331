2e4d71e2112c1ebe27ceeb20fbed71fae540ee9e
Piotr <pigryko@yahoo.com>
2021-03-31 21:28:18 +0100

    Initial commit

---
+++ models/traveller.js
@@ -0,0 +1,46 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  return this.journeys.map((journey) => {
+    return journey.startLocation;
+  })
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map((journey) => {
+    return journey.endLocation;
+  })
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+  return this.journeys.filter((journey) => {
+    return journey.transport === transport;
+  })
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  return this.journeys.filter((journey) => {
+    return journey.distance >= minDistance;
+  })
+
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  return this.journeys.reduce((total, journey) => {
+    return total += journey.distance;
+  }, 0);
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+  const all_transports = this.journeys.map((journey) => {
+    return journey.transport;
+  })
+  return all_transports.filter((transport, index) => {
+    return all_transports.indexOf(transport) === index;
+  })
+};
+
+
+module.exports = Traveller;
